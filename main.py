3551a38d4bb05861c9f3a7c4565167f59597a843
REPONCFU <stavbortsov@mail.ru>
2024-12-06 12:17:57 +0300

    Извлечены данные о широте, долготе и городе

--- main.py
+++ main.py
@@ -4,7 +4,16 @@ def get_geolocation(ip_address):
     try:
         response = requests.get(f"http://ipinfo.io/{ip_address}/json")
         data = response.json()
-        return data
+        location = data.get("loc", "").split(",")
+        latitude = location[0] if len(location) > 0 else None
+        longitude = location[1] if len(location) > 1 else None
+        city = data.get("city", "Неизвестно")
+
+        return {
+            "latitude": latitude,
+            "longitude": longitude,
+            "city": city
+        }
     except Exception as e:
         print(f"Ошибка: {e}")
         return None
