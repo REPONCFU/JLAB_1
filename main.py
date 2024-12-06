3614f377070fb6ebd66dab5569be9432b515ca7b
REPONCFU <stavbortsov@mail.ru>
2024-12-06 12:25:35 +0300

    Добавлена обработка ошибок и комментарии

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import requests
 
 def get_geolocation(ip_address):
     try:
+        # Отправка запроса к API ipinfo.io
         response = requests.get(f"http://ipinfo.io/{ip_address}/json")
         data = response.json()
+
+        # Извлечение данных о местоположении
         location = data.get("loc", "").split(",")
         latitude = location[0] if len(location) > 0 else None
         longitude = location[1] if len(location) > 1 else None
         city = data.get("city", "Неизвестно")
 
+        # Возврат результатов
         return {
             "latitude": latitude,
             "longitude": longitude,
